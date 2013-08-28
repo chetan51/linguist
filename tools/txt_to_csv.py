c996046b0c18af8a2d2ac8ecc394a46f07cf2f7f
Chetan Surpur <chetan51@gmail.com>
2013-08-28 13:57:51 -0700

    Added a tool

---
+++ tools/txt_to_csv.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# ----------------------------------------------------------------------
+# Chetan Surpur
+# Copyright (C) 2013
+# ----------------------------------------------------------------------
+
+"""A tool to convert text into csv format for Linguist."""
+
+import sys
+import sys
+import re
+
+def convert(datapath):
+    data = file(datapath).read()
+    data = re.sub(' +', ' ', data)
+    N = len(data)
+
+    print "letter"
+    print "string"
+    print ""
+
+    for i in xrange(0, N):
+        c = data[i]
+        if ord(c) > 31 and ord(c) < 127:
+            print c
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        datapath = sys.argv[1]
+        convert(datapath)
+    else:
+        print "Usage: python linguist.py [path/to/data.txt]"
